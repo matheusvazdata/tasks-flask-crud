ce693f542cbfff45c147f962d3bafbfd39d1f91d
Matheus Vaz <matheusvaz.data@gmail.com>
2025-03-30 23:32:32 -0300

    First steps to creating an API with Flask

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from models.task import Task
 
-app = Flask(__name__) # __name__ é uma variável que o Python cria automaticamente e que é igual ao nome do módulo em que está sendo usado, como __main__
+app = Flask(__name__)
 
-# Criando uma rota
-@app.route('/')
-def hello_world():
-    return 'Hello, World!' # Retorna uma string
+# CRUD -> Create, Read, Update, Delete
 
-# Criando outra rota (sobre)
-@app.route('/about')
-def about():
-    return 'Página "sobre" da aplicação'
+tasks = []
 
-# Rodando a aplicação
-if __name__ == '__main__': # Apenas executa o código abaixo se o módulo for executado manualmente, não se for importado
-    app.run(debug=True) # debug=True é um argumento que permite que a aplicação seja recarregada automaticamente sempre que houver uma alteração no código
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Test'
+
+if __name__ == '__main__': 
+    app.run(debug=True)
