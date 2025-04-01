2a4f34d69ff77f93b016a2aa6f0ccf7b96387349
Matheus Vaz <matheusvaz.data@gmail.com>
2025-04-01 01:42:18 -0300

    Code update with the insertion of the GET method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
@@ -6,12 +6,30 @@ app = Flask(__name__)
 # CRUD -> Create, Read, Update, Delete
 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({
+        'message': 'Tarefa criada com sucesso!'
+    })
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    
+    output = {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+    }
+
+    return jsonify(output)
 
 if __name__ == '__main__': 
     app.run(debug=True)
